827fe7b6301487414dc21b716463cea7b4fafb7d
TheSniker007 <joaojl2099@gmail.com>
2020-06-21 19:49:38 -0300

    gitignore edited

---
+++ build/precache-manifest.01109415d8b65a4371802481a2e3fba7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf4254b0d98b21359515711baf36a14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2b32b2b26e70b83bc3a",
+    "url": "/static/js/2.72b7db2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72b7db2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b8dbd89907ce1d4ea9",
+    "url": "/static/js/3.e10b32c3.chunk.js"
+  },
+  {
+    "revision": "f53c3d1241eb0b139e1a",
+    "url": "/static/js/4.b8d89450.chunk.js"
+  },
+  {
+    "revision": "fcf7af76a5d9ff90b7ce",
+    "url": "/static/js/5.b726bf97.chunk.js"
+  },
+  {
+    "revision": "2e705936df0bd022c5c2",
+    "url": "/static/js/main.ca7708c5.chunk.js"
+  },
+  {
+    "revision": "e17360ff68c0c5128f50",
+    "url": "/static/js/runtime-main.95c29675.js"
+  }
+]);
